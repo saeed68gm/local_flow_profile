2bb74c60c11e2df769f493c2345361aa79bbb678
saeed68gm <saeed68gm@gmail.com>
2014-10-23 22:31:33 -0400

    no compile time errors. I need to build opencv with gpu support and try it again

--- gpu_flow_based_profile.cpp
+++ gpu_flow_based_profile.cpp
@@ -18,7 +18,7 @@ using namespace std;
 
 #define N_FRAMES 50
 //#define GPU
-# define PI           3.14159265358979323846  /* pi */
+# define M_PI           3.14159265358979323846  /* pi */
 # define CUT_COLUMN 45
 // Some defines
 #define NMAX_CHARACTERS 500
@@ -31,6 +31,8 @@ const float scale_factor_ = 0.9;
 const int inner_iterations_ = 3;
 const int outer_iterations_ = 50;
 const int solver_iterations_ = 20;
+const bool resize_img = false;
+const float rfactor = 2.0;
 
 //Gray lookup table
 uchar GrayTable[256];
@@ -267,18 +269,382 @@ int modeEachRow(const Mat& input,float& mode)
 
 }
 
+//******************************************************************************
+//******************************************************************************
+
+/** This function calculates rgb values from hsv color space                     */
+void hsv2rgb(float h, float s, float v, unsigned char &r, unsigned char &g, unsigned char &b)
+{
+	float c = 0.0, hp = 0.0, hpmod2 = 0.0, x = 0.0;
+	float m = 0.0, r1 = 0.0, g1 = 0.0, b1 = 0.0;
+
+	if(h > 360)
+	{
+		h = h - 360;
+	}
+
+	c = v*s;   // chroma
+	hp = h / 60;
+	hpmod2 = hp - (float)((int)(hp/2))*2;
+
+	x = c*(1 - fabs(hpmod2 - 1));
+	m = v - c;
+
+	if(0 <= hp && hp < 1)
+	{
+		r1 = c;
+		g1 = x;
+		b1 = 0;
+	}
+	else if(1 <= hp && hp < 2)
+	{
+		r1 = x;
+		g1 = c;
+		b1 = 0;
+	}
+	else if(2 <= hp && hp < 3)
+	{
+		r1 = 0;
+		g1 = c;
+		b1 = x;
+	}
+	else if(3 <= hp && hp < 4)
+	{
+		r1 = 0;
+		g1 = x;
+		b1 = c;
+	}
+	else if(4 <= hp && hp < 5)
+	{
+		r1 = x;
+		g1 = 0;
+		b1 = c;
+	}
+	else
+	{
+		r1 = c;
+		g1 = 0;
+		b1 = x;
+	}
+
+	r = (unsigned char)(255*(r1 + m));
+	g = (unsigned char)(255*(g1 + m));
+	b = (unsigned char)(255*(b1 + m));
+}
+
+//******************************************************************************
+//******************************************************************************
+
+/** This function draws a vector field based on horizontal and vertical flow fields   */
+void drawMotionField(Mat &imgU, Mat &imgV, Mat &imgMotion,
+	int xSpace, int ySpace, float cutoff, float multiplier, CvScalar color)
+{
+	int x = 0, y = 0;
+	float *ptri;
+	float deltaX = 0.0, deltaY = 0.0, angle = 0.0, hyp = 0.0;
+	Point p0, p1;
+
+	for( y = ySpace; y < imgU.rows; y += ySpace )
+	{
+		for(x = xSpace; x < imgU.cols; x += xSpace )
+		{
+			p0.x = x;
+			p0.y = y;
+
+			ptri = imgU.ptr<float>(y);
+			deltaX = ptri[x];
+
+			ptri = imgV.ptr<float>(y);
+			deltaY = ptri[x];
+
+			angle = atan2(deltaY, deltaX);
+			hyp = sqrt(deltaX*deltaX + deltaY*deltaY);
+
+			if(hyp > cutoff)
+			{
+				p1.x = p0.x + cvRound(multiplier*hyp*cos(angle));
+				p1.y = p0.y + cvRound(multiplier*hyp*sin(angle));
+
+				cv::line(imgMotion,p0,p1,color,1,CV_AA,0);
+
+				p0.x = p1.x + cvRound(2*cos(angle-M_PI + M_PI/4));
+				p0.y = p1.y + cvRound(2*sin(angle-M_PI + M_PI/4));
+				cv::line( imgMotion, p0, p1, color,1, CV_AA, 0);
+
+				p0.x = p1.x + cvRound(2*cos(angle-M_PI - M_PI/4));
+				p0.y = p1.y + cvRound(2*sin(angle-M_PI - M_PI/4));
+				cv::line( imgMotion, p0, p1, color,1, CV_AA, 0);
+			}
+		}
+	}
+}
+
+//******************************************************************************
+//******************************************************************************
+
+/** Draws the circular legend for the color field, indicating direction and magnitude */
+void drawLegendHSV(Mat &imgColor, int radius, int px, int py)
+{
+	unsigned char *legend_ptr, *img_ptr;
+	float angle = 0.0, h = 0.0, s = 0.0, v = 0.0, legend_max_s = 0.0;
+	unsigned char r = 0, g = 0, b = 0;
+	int deltaX = 0, deltaY = 0, mod = 0;
+	int width = radius*2 + 1;
+	int height = width;
+
+	Mat imgLegend = cv::Mat::zeros(Size(width,height),CV_8UC3);
+
+	legend_max_s = radius*sqrt((float)2);
+
+	for( int y = 0; y < imgLegend.rows; y++ )
+	{
+		legend_ptr = imgLegend.ptr<unsigned char>(y);
+
+		for( int x = 0; x < imgLegend.cols; x++ )
+		{
+			deltaX = x-radius;
+			deltaY = -(y-radius);
+			angle = atan2((float)deltaY,deltaX);
+
+			if( angle < 0.0 )
+			{
+				angle += 2*M_PI;
+			}
+
+			h = angle * 180.0 / M_PI;
+			s = sqrt((float)deltaX*deltaX + deltaY*deltaY) / legend_max_s;
+			v = 0.9;
+
+			hsv2rgb(h,s,v,r,g,b);
+
+			legend_ptr[3*x] = b;
+			legend_ptr[3*x+1] = g;
+			legend_ptr[3*x+2] = r;
+
+			mod = (x-radius)*(x-radius) + (y-radius)*(y-radius);
+
+			if(  mod < radius*radius )
+			{
+				img_ptr = imgColor.ptr<unsigned char>(py+y);
+				img_ptr[3*(px+x)] = legend_ptr[3*x];
+				img_ptr[3*(px+x)+1] = legend_ptr[3*x+1];
+				img_ptr[3*(px+x)+2] = legend_ptr[3*x+2];
+			}
+		}
+	}
+
+	// Draw a black circle over the legend
+	cv::circle(imgColor,cv::Point(px+radius,py+radius),radius,CV_RGB(0,0,0),2,8,0);
+}
+
+//******************************************************************************
+//******************************************************************************
+
+/** This function draws a color field representation of the flow field          */
+void drawColorField(Mat &imgU, Mat &imgV, Mat &imgColor)
+{
+	Mat imgColorHSV = cv::Mat::zeros(Size(imgColor.cols,imgColor.rows),CV_32FC3);
+
+	float max_s = 0;
+	float *hsv_ptr, *u_ptr, *v_ptr;
+	unsigned char *color_ptr;
+	unsigned char r = 0, g = 0, b = 0;
+	float angle = 0.0;
+	float h = 0.0, s = 0.0, v = 0.0;
+	float deltaX = 0.0, deltaY = 0.0;
+	int x = 0, y = 0;
+
+	// Generate hsv image
+	for( y = 0; y < imgColor.rows; y++ )
+	{
+		hsv_ptr = imgColorHSV.ptr<float>(y);
+		u_ptr = imgU.ptr<float>(y);
+		v_ptr = imgV.ptr<float>(y);
+
+		for( x = 0; x < imgColorHSV.cols; x++)
+		{
+			deltaX = u_ptr[x];
+			deltaY = -v_ptr[x];
+			angle = atan2(deltaY,deltaX);
+
+			if( angle < 0)
+			{
+				angle += 2*M_PI;
+			}
+
+			hsv_ptr[3*x] = angle * 180 / M_PI;
+			hsv_ptr[3*x+1] = sqrt(deltaX*deltaX + deltaY*deltaY);
+			hsv_ptr[3*x+2] = 0.9;
+
+			if( hsv_ptr[3*x+1] > max_s )
+			{
+				max_s = hsv_ptr[3*x+1];
+			}
+		}
+	}
+
+	// Generate color image
+	for(y = 0; y < imgColor.rows; y++ )
+	{
+		hsv_ptr = imgColorHSV.ptr<float>(y);
+		color_ptr = imgColor.ptr<unsigned char>(y);
+
+		for( x = 0; x < imgColor.cols; x++)
+		{
+			h = hsv_ptr[3*x];
+			s = hsv_ptr[3*x+1] / max_s;
+			v = hsv_ptr[3*x+2];
+
+			hsv2rgb(h,s,v,r,g,b);
+
+			color_ptr[3*x] = b;
+			color_ptr[3*x+1] = g;
+			color_ptr[3*x+2] = r;
+		}
+	}
+
+	drawLegendHSV(imgColor,15,25,15);
+}
+
 int main()   
 {
 	// Variables for CUDA Brox Optical flow
-	GpuMat frame0GPU, frame1GPU, uGPU, vGPU;
+	gpu::GpuMat frame0GPU, frame1GPU, uGPU, vGPU;
 	Mat frame0_rgb_, frame1_rgb_, frame0_rgb, frame1_rgb, frame0, frame1;
 	Mat frame0_32, frame1_32, imgU, imgV;
 	Mat motion_flow, flow_rgb;
 	int nframes = 0, width = 0, height = 0;
 	char cad[NMAX_CHARACTERS];
-	//the array to keep track of local max flows
-	//list<double,int> local_max_list;
-	//double local_max=0;
+
+	// Variables for measuring computation times
+	double t1 = 0.0, t2 = 0.0, tdflow = 0.0, tvis = 0.0;
+
+	// Show CUDA information
+	cv::gpu::printShortCudaDeviceInfo(cv::gpu::getDevice());
+
+	// Create OpenCV windows
+	namedWindow("Dense Flow",CV_WINDOW_OPENGL);
+	namedWindow("Motion Flow",CV_WINDOW_NORMAL);
+
+	// Create the optical flow object
+	cv::gpu::BroxOpticalFlow dflow(alpha_,gamma_,scale_factor_,inner_iterations_,outer_iterations_,solver_iterations_);
+
+	// Open the video file
+	VideoCapture cap =VideoCapture("16_cut_small.avi");
+
+	if( cap.isOpened() == 0 )
+	{
+		return -1;
+	}
+	cap >> frame1_rgb_;
+
+	if( resize_img == true )
+	{
+		frame1_rgb = cv::Mat(Size(cvRound(frame1_rgb_.cols/rfactor),cvRound(frame1_rgb_.rows/rfactor)),CV_8UC3);
+		width = frame1_rgb.cols;
+		height = frame1_rgb.rows;
+		cv::resize(frame1_rgb_,frame1_rgb,cv::Size(width,height),0,0,INTER_LINEAR);
+	}
+	else
+	{
+		frame1_rgb = cv::Mat(Size(frame1_rgb_.cols,frame1_rgb_.rows),CV_8UC3);
+		width = frame1_rgb.cols;
+		height = frame1_rgb.rows;
+		frame1_rgb_.copyTo(frame1_rgb);
+	}
+
+	// Allocate memory for the images
+	frame0_rgb = cv::Mat(Size(width,height),CV_8UC3);
+	flow_rgb = cv::Mat(Size(width,height),CV_8UC3);
+	motion_flow = cv::Mat(Size(width,height),CV_8UC3);
+	frame0 = cv::Mat(Size(width,height),CV_8UC1);
+	frame1 = cv::Mat(Size(width,height),CV_8UC1);
+	frame0_32 = cv::Mat(Size(width,height),CV_32FC1);
+	frame1_32 = cv::Mat(Size(width,height),CV_32FC1);
+
+	// Convert the image to grey and float
+	cvtColor(frame1_rgb,frame1,CV_BGR2GRAY);
+	frame1.convertTo(frame1_32,CV_32FC1,1.0/255.0,0);
+
+	while( frame1.empty() == false )
+	{
+		if( nframes >= 1 )
+		{
+			// Upload images to the GPU
+			frame1GPU.upload(frame1_32);
+			frame0GPU.upload(frame0_32);
+
+			// Do the dense optical flow
+			dflow(frame0GPU,frame1GPU,uGPU,vGPU);
+
+			uGPU.download(imgU);
+			vGPU.download(imgV);
+
+		}
+
+		if( nframes >= 1 )
+		{
+
+			// Draw the optical flow results
+			drawColorField(imgU,imgV,flow_rgb);
+
+			frame1_rgb.copyTo(motion_flow);
+			drawMotionField(imgU,imgV,motion_flow,15,15,.0,3,CV_RGB(0,255,0));
+
+			// Visualization
+			imshow("Dense Flow",flow_rgb);
+			imshow("Motion Flow",motion_flow);
+
+			waitKey(3);
+
+		}
+
+		// Save results
+		if( SAVE_RESULTS == true )
+		{
+			sprintf(cad,"./output/motion/image%04d.jpg",nframes);
+			imwrite(cad,motion_flow);
+
+			sprintf(cad,"./output/flow/image%04d.jpg",nframes);
+			imwrite(cad,flow_rgb);
+		}
+
+		// Set the information for the previous frame
+		frame1_rgb.copyTo(frame0_rgb);
+		cvtColor(frame0_rgb,frame0,CV_BGR2GRAY);
+		frame0.convertTo(frame0_32,CV_32FC1,1.0/255.0,0);
+
+		// Read the next frame
+		nframes++;
+		cap >> frame1_rgb_;
+
+		if( frame1_rgb_.empty() == false )
+		{
+			if( resize_img == true )
+			{
+				cv::resize(frame1_rgb_,frame1_rgb,cv::Size(width,height),0,0,INTER_LINEAR);
+			}
+			else
+			{
+				frame1_rgb_.copyTo(frame1_rgb);
+			}
+
+			cvtColor(frame1_rgb,frame1,CV_BGR2GRAY);
+			frame1.convertTo(frame1_32,CV_32FC1,1.0/255.0,0);
+		}
+		else
+		{
+			break;
+		}
+
+		cout << "Frame Number: " << nframes << endl;
+		cout << "Time Dense Flow: " << tdflow << endl;
+		cout << "Time Visualization: " << tvis << endl << endl;
+	}
+
+	// Destroy the windows
+	destroyAllWindows();
+
 
 	Mat FG;
 	FG=imread("FG0.tiff",0);
@@ -287,73 +653,5 @@ int main()
 		cout<<"could not load foreground model!"<<endl;
 		return 0;
 	}
-
-	VideoCapture video =VideoCapture("16_cut_small.avi");
-	VideoWriter videoOut;
-	videoOut.open("major_flow.avi", CV_FOURCC('X','V','I','D'), video.get(CV_CAP_PROP_FPS), Size(video.get(CV_CAP_PROP_FRAME_WIDTH),video.get(CV_CAP_PROP_FRAME_HEIGHT)), true);
-	if (!videoOut.isOpened())
-	{
-		cout  << "Could not open the output video for write! "  << endl;
-		return -1;
-	}
-	double total_frames;
-	total_frames=video.get(CV_CAP_PROP_FRAME_COUNT);
-	video>>showImg;
-	Mat mask_img = Mat(FG.rows,FG.cols,CV_8U);
-	mask_img.setTo(0);
-	Mat mask_long=Mat(FG.rows*2,FG.cols/2,CV_8U);
-	mask_long.setTo(0);
-	temp_mat=showImg.clone();
-	Rect roi=Rect(200,0,100,temp_mat.rows);
-	//curFrame=temp_mat(Rect(3*temp_mat.cols/4,0,temp_mat.cols/4,temp_mat.rows));
-	curFrame=temp_mat(roi);
-	cv::cvtColor(curFrame,lastGrey,CV_BGR2GRAY);
-	//initialize the motion condensed image
-	motionCondensed=Mat(showImg.rows,(int)total_frames,CV_8UC3);
-	double displacementX=0, displacementY=0;
-	int frameCounter=1;
-	char key=0;
-	
-	while(key!=27)
-	{
-		video>>showImg;
-		if (showImg.empty())
-		{
-			break;
-		}
-		temp_mat=showImg.clone();
-		curFrame=temp_mat(roi);
-		cv::cvtColor(curFrame,curGrey,CV_BGR2GRAY);
-		calcOpticalFlowFarneback(lastGrey,curGrey,flow,0.5,1,10,10,25,2.0,2 );
-		gpu::BroxOpticalFlow dflow(alpha_,gamma_,scale_factor_,inner_iterations_,outer_iterations_,solver_iterations_);
-// 		float a,b;
-// 		find_mat_max(flow,a,b);
-		//drawOptFlowMap(flow,curFrame,8, CV_RGB(0, 255, 0));
-		OptFlowMagColorVis(flow,curGrey);
-		imshow("optical flow magnitude",curGrey);
-		//save_x_component(flow,mask_img,frameCounter, FG,CUT_COLUMN);
-		x_component(flow,mask_long,frameCounter);
-		//drawOptFlowColumn(flow,curFrame,CUT_COLUMN,1, CV_RGB(0, 255, 0));
-		drawOptFlowColumn(flow,curFrame,CUT_COLUMN-12,CUT_COLUMN+12,1, CV_RGB(0, 255, 0));
-		imshow("optical flow",curFrame);
-		waitKey(120);
-		frameCounter++;
-
-	}
-	
-	//mask_img=imread("mask - Copy.tiff",0);
-// 	bitwise_and(mask_img,FG,mask_img);
- 	Mat temp_mask=Mat(mask_img);
-// 	normalize(mask_img,temp_mask,255,0,CV_MINMAX);
-// 	//GaussianBlur(temp_mask,mask_img, Size(5,5),1.5);
-// 	imwrite("mask.tiff",temp_mask);
-	//mask_long=imread("mask-long-edited.tiff",0);
-	temp_mask=Mat(mask_long);
-	image_normalize(mask_long,temp_mask,255,0);
-	resize(temp_mask,mask_img,mask_img.size	());
-	bitwise_and(mask_img,FG,temp_mask);
-	
-	imwrite("mask-long.tiff",temp_mask);
-	waitKey();
 	return 0;
 }
